989a266edd947b30cd2ade2e88558c404450dab2
LuisArturoM <119456723+LuisArturoM@users.noreply.github.com>
2025-07-23 17:55:04 -0600

    Added Cards/aboutus/contact/backbtn

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,21 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import type { Metadata } from 'next';
-import { getValuesIcea } from '@/lib/data';
 import ValuesCard from './components/ui/ValuesCard';
 import { SucursalCardContainer } from './components/ui/SucursalCard/SucursalCardContainer';
 import MainPageContentContainer from './components/ui/MainPageContentContainer';
+import AboutICEA from './components/ui/AboutUs';
+import ContactSection from './components/ui/Contact';
+import BackToTopButton from './components/ui/BackToTopBtn';
 
 export const metadata: Metadata = {
   title: 'Inicio',
 };
 
 export default async function HomePage() {
-  const valuesicea = await getValuesIcea();
+  const valuesicea = await import('@/lib/data').then((data) => data.getValuesIcea());
+  const companies = await import('@/lib/data').then((data) => data.getCompanies());
+  const iceastats = await import('@/lib/data').then((data) => data.getIceaStats());
 
   return (
     <section className="flex min-h-[calc(100vh-10rem)] flex-col items-center justify-center text-center bg-gradient-to-b from-[#F8D8B5]/10 via-[#F8D8B5]/50 to-[#F8D8B5]">
@@ -41,6 +45,20 @@ export default async function HomePage() {
           values={valuesicea}
         />
       </MainPageContentContainer>
+      <MainPageContentContainer>
+        <AboutICEA
+          title="Sobre Grupo ICEA"
+          description={`Grupo ICEA es una corporación mexicana fundada con el propósito de revolucionar la industria alimentaria a través de la innovación, calidad y excelencia operativa.\n\nNuestro portafolio incluye empresas líderes en diferentes segmentos del sector alimentario, desde carnicerías especializadas hasta distribución de alimentos frescos, siempre manteniendo nuestro compromiso con la calidad y la satisfacción del cliente.`}
+          stats={iceastats}
+          companies={companies}
+        />
+      </MainPageContentContainer>
+
+      <MainPageContentContainer>
+        <ContactSection />
+      </MainPageContentContainer>
+
+      <BackToTopButton />
     </section>
   );
 }
