f36a1dec1952a1d007640ddba31913540f7b09b0
Christian <misa_936@hotmail.com>
2025-07-23 22:36:39 -0600

    chore(): fix about style and change imports

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,15 +7,16 @@ import MainPageContentContainer from './components/ui/MainPageContentContainer';
 import AboutICEA from './components/ui/AboutUs';
 import ContactSection from './components/ui/Contact';
 import BackToTopButton from './components/ui/BackToTopBtn';
+import { getCompanies, getIceaStats, getValuesIcea } from '@/lib/data';
 
 export const metadata: Metadata = {
   title: 'Inicio',
 };
 
 export default async function HomePage() {
-  const valuesicea = await import('@/lib/data').then((data) => data.getValuesIcea());
-  const companies = await import('@/lib/data').then((data) => data.getCompanies());
-  const iceastats = await import('@/lib/data').then((data) => data.getIceaStats());
+  const valuesicea = await getValuesIcea();
+  const companies = await getCompanies();
+  const iceastats = await getIceaStats();
 
   return (
     <section className="flex min-h-[calc(100vh-10rem)] flex-col items-center justify-center text-center bg-gradient-to-b from-[#F8D8B5]/10 via-[#F8D8B5]/50 to-[#F8D8B5]">
@@ -45,14 +46,12 @@ export default async function HomePage() {
           values={valuesicea}
         />
       </MainPageContentContainer>
-      <MainPageContentContainer>
-        <AboutICEA
-          title="Sobre Grupo ICEA"
-          description={`Grupo ICEA es una corporación mexicana fundada con el propósito de revolucionar la industria alimentaria a través de la innovación, calidad y excelencia operativa.\n\nNuestro portafolio incluye empresas líderes en diferentes segmentos del sector alimentario, desde carnicerías especializadas hasta distribución de alimentos frescos, siempre manteniendo nuestro compromiso con la calidad y la satisfacción del cliente.`}
-          stats={iceastats}
-          companies={companies}
-        />
-      </MainPageContentContainer>
+      <AboutICEA
+        title="Sobre Grupo ICEA"
+        description={`Grupo ICEA es una corporación mexicana fundada con el propósito de revolucionar la industria alimentaria a través de la innovación, calidad y excelencia operativa.\n\nNuestro portafolio incluye empresas líderes en diferentes segmentos del sector alimentario, desde carnicerías especializadas hasta distribución de alimentos frescos, siempre manteniendo nuestro compromiso con la calidad y la satisfacción del cliente.`}
+        stats={iceastats}
+        companies={companies}
+      />
 
       <MainPageContentContainer>
         <ContactSection />
