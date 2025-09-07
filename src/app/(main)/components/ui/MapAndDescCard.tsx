decadcc0ace60fe2b39857bc7cc7d0b00ec0e119
ferfidelrod <ferfidelrod@gmail.com>
2025-09-07 17:19:42 -0600

    seccion de tienda agregada

--- src/app/(main)/components/ui/MapAndDescCard.tsx
+++ src/app/(main)/components/ui/MapAndDescCard.tsx
@@ -4,20 +4,12 @@ import { LucideIcon, Award, Users } from 'lucide-react';
 type Props = {
   imageSrc: string;
   description: React.ReactNode;
-  mapEmbedUrl: string;
   sucursalId: number;
   firstLogoText: string;
   secondLogoText: string;
 };
 
-export default function MapAndDesc({
-  imageSrc,
-  description,
-  mapEmbedUrl,
-  sucursalId,
-  firstLogoText,
-  secondLogoText,
-}: Props) {
+export default function MapAndDesc({ imageSrc, description, sucursalId, firstLogoText, secondLogoText }: Props) {
   let icon1: LucideIcon;
   let icon2: LucideIcon;
   icon1 = Award;
@@ -56,7 +48,7 @@ export default function MapAndDesc({
   const Icon1 = icon1;
   const Icon2 = icon2;
   return (
-    <section className="bg-[#FFF8E7] px-6 py-12">
+    <section className="bg-[#FFFFFF] px-6 py-12">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-10 items-start md:items-center md:justify-between">
         {/* Texto y descripción */}
         <div className="flex-1 space-y-6 w-full max-w-xl">
@@ -80,15 +72,6 @@ export default function MapAndDesc({
         <div className="flex flex-col gap-4 flex-1">
           <div className="rounded-xl overflow-hidden shadow-md">
             <Image src={imageSrc} alt="Carnicería" width={640} height={360} className="object-cover w-full h-auto" />
-          </div>
-          <div className="rounded-xl overflow-hidden shadow-md">
-            <iframe
-              src={mapEmbedUrl}
-              className="w-full h-[150px] sm:h-[200px] md:h-[250px] lg:h-[300px] border-0 rounded-xl"
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            />
           </div>
         </div>
       </div>
--- src/app/sucursales/[slug]/page.tsx
+++ src/app/sucursales/[slug]/page.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from 'next';
 import { SucursalPageWrapper } from '../components/SucursalPageWrapper';
 import { getImagesFromFolder } from '@/lib/cloudinary';
 import MapAndDesc from '@/app/(main)/components/ui/MapAndDescCard';
-import { Clock } from 'lucide-react';
 import Servicios from '../components/ui/Servicios';
+import VisitaNuestraTienda from '../components/ui/VisitaNuestraTienda';
 import CarouselContainer from '../components/ui/CarouselContainer';
 
 export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
@@ -49,23 +49,11 @@ export default async function SucursalPage({ params }: { params: Promise<{ slug:
       <MapAndDesc
         imageSrc={sucursal.fachada}
         description={sucursal.descripcion}
-        mapEmbedUrl={sucursal.map}
         sucursalId={sucursal.id}
         firstLogoText={sucursal.descFirstLogotext}
         secondLogoText={sucursal.descSecondLogotext}
       />
-      <div className="max-w-7xl mx-auto px-6">
-        <div className="ml-4 md:ml-8 lg:ml-12">
-          <div className="space-y-2 mt-4">
-            <div className="flex items-center text-lg text-neutral-700">
-              <Clock className="h-5 w-5 text-orange-500 mr-2" />
-              <span>
-                <span className="font-semibold text-orange-500">Horario:</span> {sucursal.horario}
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <VisitaNuestraTienda />
       <Servicios />
     </SucursalPageWrapper>
   );
